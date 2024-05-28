d34922360e00a50543a91828066eb97546b9e5e2
headlessNode <harriskhan047@outlook.com>
2024-05-28 19:40:28 +0500

    Fix loading animation

--- src/cards.jsx
+++ src/cards.jsx
@@ -8,21 +8,21 @@ function LoadingSpinner() {
 
     useGSAP(() => {
         tl.current = gsap
-            .timeline()
-            .to('.circle-one', { y: -30, background: '#000', duration: 0.15 })
-            .to('.circle-one', { y: 0, background: '#ccc', duration: 0.15 })
+            .timeline({ repeat: -1 })
+            .to('.circle-one', { y: -30, background: '#000', duration: 0.25 })
+            .to('.circle-one', { y: 0, background: '#ccc', duration: 0.25 })
             .to(
                 '.circle-two',
-                { y: -30, background: '#000', duration: 0.15 },
+                { y: -30, background: '#000', duration: 0.25 },
                 '<'
             )
-            .to('.circle-two', { y: 0, background: '#ccc', duration: 0.15 })
+            .to('.circle-two', { y: 0, background: '#ccc', duration: 0.25 })
             .to(
                 '.circle-three',
-                { y: -30, background: '#000', duration: 0.15 },
+                { y: -30, background: '#000', duration: 0.25 },
                 '<'
             )
-            .to('.circle-three', { y: 0, background: '#ccc', duration: 0.15 })
+            .to('.circle-three', { y: 0, background: '#ccc', duration: 0.25 })
     }, [container])
 
     return (
